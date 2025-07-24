160099eb8d571a6f09eec8c3cd26b497b23a4fb5
Simar Singh Sandhu <sandhusimarsingh@gmail.com>
2025-07-23 23:27:22 -0400

    feat: env update to live backend

--- src/constants.js
+++ src/constants.js
@@ -12,4 +12,4 @@ export const ROUTES = {
   [REPORTS]: "/reports",
 }
 
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"
+export const API_URL = process.env.REACT_APP_API_URL
